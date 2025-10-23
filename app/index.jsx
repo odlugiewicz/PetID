ade611b9b36826c61b03dd7223918515e5357f3f
odlugiewicz <odlugiewicz.9@gmail.com>
2025-10-23 16:32:59 +0200

    Created about and contact pages

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import {Link} from 'expo-router'
+
+import Logo from '../assets/img/logo_blue.png'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+
+      <Image source={Logo} style={styles.img}/>
+
+      <Text style={styles.title}>Keep Your Pets Info with You!</Text>
+
+      <Text style={{marginTop: 10, marginBottom: 30}}>
+        Join Now!
+      </Text>
+
+      <Link href="/about" style={styles.link}>About Page</Link>
+      <Link href="/contact" style={styles.link}>Contact Page</Link>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  img: {
+    marginVertical: 20
+  },
+  link: {
+    marginVertical: 20,
+    borderBottomWidth: 1
+  }
+})
