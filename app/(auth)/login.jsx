e51c1f98199abc8e18505ea0d818690bd455be17
odlugiewicz <odlugiewicz.9@gmail.com>
2025-10-29 16:06:54 +0100

    Connected to Appwrite, created TheamedTextInput component and put it in login and register pages

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,36 +1,64 @@
-import { StyleSheet, Pressable, Text } from 'react-native'
+import { StyleSheet, Text, Image, TouchableWithoutFeedback, Keyboard} from 'react-native'
 import { Link } from 'expo-router'
 import { Colors } from '../../constants/Colors'
+import { useState } from 'react'
 
+
+import Logo from '../../assets/img/logo_blue.png'
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
 import ThemedButton from '../../components/ThemedButton'
+import ThemedTextInput from '../../components/ThemedTextInput'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     const handleSubmit = () => {
-        console.log("Sign In pressed")
+        console.log("Sign In form subbmitted", email, password)
     }
+
     return (
-        <ThemedView style={styles.container}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <ThemedView style={styles.container}>
+                <Image source={Logo} />
+                <Spacer height={20} />
 
-            <Spacer />
-            <ThemedText title={true} style={styles.title}>
-                Sign In
-            </ThemedText>
-
-            <ThemedButton onPress={handleSubmit}>
-                <Text style={{ color: '#F5FCFA'}}> Sign In</Text>
-            </ThemedButton>
-
-            <Spacer height={100} />
-            <Link href="/register" >
-                <ThemedText style={{ textAlign: "center" }}>
-                    Don't have an account? Sign Up
+                <Spacer />
+                <ThemedText title={true} style={styles.title}>
+                    Sign In to Your Account
                 </ThemedText>
-            </Link>
 
-        </ThemedView>
+                <ThemedTextInput
+                    style={{ width: '80%', marginBottom: 20 }}
+                    placeholder="Email"
+                    keyboardType="email-address"
+                    onChangeText={setEmail}
+                    value={email}
+                />
+
+                <ThemedTextInput
+                    style={{ width: '80%', marginBottom: 20 }}
+                    placeholder="Password"
+                    onChangeText={setPassword}
+                    value={password}
+                    secureTextEntry={true}
+                />
+
+                <ThemedButton onPress={handleSubmit}>
+                    <Text style={{ color: '#F5FCFA' }}> Sign In</Text>
+                </ThemedButton>
+
+                <Spacer height={40} />
+                <Link href="/register" >
+                    <ThemedText style={{ textAlign: "center" }}>
+                        Don't have an account? Sign Up
+                    </ThemedText>
+                </Link>
+
+            </ThemedView>
+        </TouchableWithoutFeedback>
     )
 }
 
--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,35 +1,65 @@
-import { StyleSheet, Text} from 'react-native'
+import { StyleSheet, Text, Image, TouchableWithoutFeedback, Keyboard} from 'react-native'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
+import { useState } from 'react'
 
+
+import Logo from '../../assets/img/logo_blue.png'
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
 import ThemedButton from '../../components/ThemedButton'
+import ThemedTextInput from '../../components/ThemedTextInput'
 
 const Register = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     const handleSubmit = () => {
-        console.log("Sign Up pressed")
+        console.log("Sign Up from submitted", email, password)
     }
     return (
-        <ThemedView style={styles.container}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <ThemedView style={styles.container}>
 
-            <Spacer />
-            <ThemedText title={true} style={styles.title}>
-                Sign Up for PetID
-            </ThemedText>
+                <Image source={Logo} />
+                <Spacer height={20} />
 
-            <ThemedButton onPress={handleSubmit}>
-                <Text style={{ color: '#F5FCFA' }}> Sign Up</Text>
-            </ThemedButton>
-
-            <Spacer height={100} />
-            <Link href="/login" >
-                <ThemedText style={{ textAlign: "center" }}>
-                    Already have an account? Sign In
+                <Spacer />
+                <ThemedText title={true} style={styles.title}>
+                    Sign Up for PetID
                 </ThemedText>
-            </Link>
 
-        </ThemedView>
+                <ThemedTextInput
+                    style={{ width: '80%', marginBottom: 20 }}
+                    placeholder="Email"
+                    keyboardType="email-address"
+                    onChangeText={setEmail}
+                    value={email}
+                />
+
+                <ThemedTextInput
+                    style={{ width: '80%', marginBottom: 20 }}
+                    placeholder="Password"
+                    onChangeText={setPassword}
+                    value={password}
+                    secureTextEntry={true}
+                />
+
+
+                <ThemedButton onPress={handleSubmit}>
+                    <Text style={{ color: '#F5FCFA' }}> Sign Up</Text>
+                </ThemedButton>
+
+                <Spacer height={40} />
+                <Link href="/login" >
+                    <ThemedText style={{ textAlign: "center" }}>
+                        Already have an account? Sign In
+                    </ThemedText>
+                </Link>
+
+            </ThemedView>
+        </TouchableWithoutFeedback>
     )
 }
 
